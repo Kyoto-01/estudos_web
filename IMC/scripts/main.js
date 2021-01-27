494c912e1622e2a7b1991bf9b06db308a9262e6f
Jônatas Mendes da Cruz <62781857+Kyoto-01@users.noreply.github.com>
2021-01-27 15:53:17 -0300

    fix bugs
    bugs in #resultado visibility are fixed :)

--- IMC/scripts/main.js
+++ IMC/scripts/main.js
@@ -11,16 +11,18 @@ function verificarFormulario(){
 	let entradaPeso = document.getElementById("peso");
 	let altura = parseFloat(entradaAltura.value);
 	let peso = parseFloat(entradaPeso.value);
-	
+
 	new IMC().atribuirIMC(altura, peso);
 	location = "#resultado";
 }
 
 function mostrarResultado(cor, descricao, imgLink){
+	let resultadoDiv = document.getElementById("resultado");
 	let containerResultado = document.getElementById("container-resultado");
 	let resultImg = containerResultado.querySelector("img");
 	let resultDesc = containerResultado.querySelector("p");
 	
+	resultadoDiv.style.display = "block";
 	resultImg.src = imgLink;
 	resultDesc.textContent = descricao;
 	resultDesc.style.backgroundColor = cor;
